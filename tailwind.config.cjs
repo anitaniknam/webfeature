2d450a20dde8b557a6576de7ba8e05d932b815e7
”Christopher <chri26kx@stud.kea.dkgit config --global user.email chri26kx@stud.kea.dk>
2022-09-12 22:14:32 +0200

    vores farver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        featureGrey: "#E8E8E8",
+        featureWhite: "#F6F4EF",
+        featureOrange: "#EF7504",
+        featureBlue: "#032CA6",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
