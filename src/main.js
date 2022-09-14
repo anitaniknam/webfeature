88f1f2ff10d0c1bdb0d6c712dce0fcf1739ac5a0
Anita Niknam <anita.niknam@gmail.com>
2022-09-14 14:12:16 +0200

    animationer

--- src/main.js
+++ src/main.js
@@ -5,3 +5,26 @@ import { animate, stagger, inView, scroll, timeline } from "motion";
 inView(".fotoinview", () => {
   animate("#Pierre", { opacity: [0, 1] }, { duration: 10 });
 });
+
+inView(".fotoinview", () => {
+  animate(
+    "#staggeranimation img",
+    { opacity: [0, 1] },
+    {
+      delay: stagger(1, { start: 2 }),
+    }
+  );
+});
+
+inView(".inviewnotinview", ({ target }) => {
+  animate(target, { rotate: 90 }, { delay: 1, duration: 2 });
+  return () => {
+    animate(target, { rotate: 0 });
+  };
+});
+
+const zoomeffekt = [
+  [".zoomeffekt", { transform: "scale(1.5)" }, { duration: 0.5 }],
+  [".zoomeffekt", { transform: "scale(1)" }, { duration: 1 }],
+];
+timeline(zoomeffekt);
