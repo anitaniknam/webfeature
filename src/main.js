c41495963c964f4f43ee19a02db0773d968ebd9e
Anita Niknam <anita.niknam@gmail.com>
2022-09-14 13:44:53 +0200

    pierrefadein

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+inView(".fotoinview", () => {
+  animate("#Pierre", { opacity: [0, 1] }, { duration: 10 });
+});
